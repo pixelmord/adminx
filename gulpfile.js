6c07f486b829f6190dc7979bb00665042d01ea52
Andreas Sahle <andreas.sahle@wunderkraut.com>
2016-01-18 16:10:44 +0100

    remove deprecated postcss dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var gulpConfig = require('./gulpconfig');
 var path = require('path');
 
 // Gulp tasks
-require('gulp-task-postcss')(gulp, gulpConfig);
 require('gulp-task-lint-browser')(gulp, gulpConfig);
 require('gulp-task-css-inject')(gulp, gulpConfig);
 require('gulp-task-sass')(gulp, gulpConfig);
